d81ed0596e750cbe8b21227c4dffeddb932ce92f
Aleff Moura <aleff_moura@hotmail.com>
2025-01-26 20:26:16 +0000

    created base for arch in frontend, just the project and 2 components for list orders and product

---
+++ Frontend/growin-app/src/app/components/products/product-list.tsx
@@ -0,0 +1,53 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(
+  id: number,
+  name: string,
+  quantity: number
+) {
+  return { id, name, quantity};
+}
+
+const rows = [
+  createData(1, 'Product 1', 100),
+  createData(2, 'Product 2', 100),
+  createData(3, 'Product 3', 100),
+];
+
+export default function ProductsComponent() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 550 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell align="right">Nome</TableCell>
+            <TableCell align="right">Quantidade em stock</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }} >
+              <TableCell component="th" scope="row">
+                {row.id}
+              </TableCell>
+              <TableCell align="right">{row.name}</TableCell>
+              <TableCell align="right">{row.quantity}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
